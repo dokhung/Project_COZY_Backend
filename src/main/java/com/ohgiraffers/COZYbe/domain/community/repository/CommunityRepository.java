a798433e77dc1909374648345aff027640f7137e
dokhung <ehhiop123@naver.com>
2025-08-17 17:52:49 +0900

    feat/ver1.0

--- src/main/java/com/ohgiraffers/COZYbe/domain/community/repository/CommunityRepository.java
+++ src/main/java/com/ohgiraffers/COZYbe/domain/community/repository/CommunityRepository.java
@@ -2,12 +2,22 @@ package com.ohgiraffers.COZYbe.domain.community.repository;
 
 import com.ohgiraffers.COZYbe.domain.community.entity.Community;
 import com.ohgiraffers.COZYbe.domain.plan.entity.Plan;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 public interface CommunityRepository extends JpaRepository<Community, Long> {
     List<Community> findAllByNickname(String nickname);
+    @Transactional
+    void deleteAllByProject_ProjectId(Long projectId);
+
+    @Transactional
+    void deleteAllByUser_UserId(UUID userId);
+
+    @Transactional
+    void deleteAllByProject_ProjectIdIn(List<Long> projectIds);
 }
